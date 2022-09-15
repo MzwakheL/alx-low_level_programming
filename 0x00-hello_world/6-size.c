d6c95d8d55fb3d57efb1c5c931175b2c6c2de203
Mzwakhe Louw <mzwakhe.louw@gmail.com>
2022-09-15 04:58:56 -0700

    Size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,12 +6,11 @@
  */
 int main(void)
 {
-    i; // use i for nothing
-    (int)i; // convert the value of i to integer, than use that value for nothing
-    (int)(float)i; // convert to float, then to int, then use for nothing
-    (int)(float)(char)i; // convert char, then to float, then to int, then use for nothing
-    printf("sizeof i is %d\n", (int)sizeof i);
-    char i; // define a new i (and hide the previous one) of type char
-    printf("sizeof i is %d\n", (int)sizeof i);
+	printf("Size of a char: %d byte(s)\n", sizeof(char));
+	printf("Size of an int: %d byte(s)\n", sizeof(int));
+	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %d byte(s)\n", sizeof(float));
+	return (0);
 }
 
