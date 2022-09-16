9570dd400fba6de4e6ff9d3cb8f3d1f44083422a
Mzwakhe Louw <mzwakhe.louw@gmail.com>
2022-09-16 15:04:13 -0700

    Main

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -7,18 +7,26 @@
 
 int main(void)
 {
-	int i;
-	printf("Infinite loop incoming :(n\");
-	
-	i = 0;
+        int i;
+        printf("Infinite loop incoming :(n\");
 
-	while (i < 10)
-	{
-		putchar(i);
-	}
-	
-	printf("Infinite loop avoided! \\o/\n");
+        i = 0;
 
-	return (0);
+        while (i < 10)
+        {
+                putchar(i);
+        }
 
+        printf("Infinite loop avoided! \\o/\n");
+
+        i = 0;
+
+        /* while (i < 10) */
+        /* { */
+                /* putchar(i); */
+        /* } */
+
+        return (0);
+
+}
 }
